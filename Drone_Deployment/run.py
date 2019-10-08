8dde6b381d7835ec507875b0f7626a8845591f3e
Shradha <shradha.sgl@gmail.com>
2019-10-08 12:29:02 +0530

    Drone Deployment code made more modular

--- Drone_Deployment/run.py
+++ Drone_Deployment/run.py
@@ -5,7 +5,6 @@ import requests
 
 app = Flask(__name__)
 
-# lis = [[17.4486, 78.3908], [17.4399, 78.4983], [17.3616, 78.4747], [17.3833, 78.4011], [17.2403, 78.4294]]
 lis_final = [
                 {"place": "Madhapur", "cood": [17.4486, 78.3908]}, 
                 {"place": "Secundarabad","cood": [17.4399, 78.4983]}, 
@@ -13,7 +12,7 @@ lis_final = [
                 {"place": "Golconda fort", "cood" :[17.3833, 78.4011]}, 
                 {"place":"RGIA airport","cood": [17.2403, 78.4294]} 
             ]
-# traffic = []'
+
 rain  = []
 i = 0
 
@@ -39,16 +38,14 @@ def new():
             print(rain[i])
             if i == 0:
                 ans = i
-                ans_max = int(rain[i])/2 - int(lis_final[i]["traffic"])/2
+                ans_max = (int(rain[i]) - int(lis_final[i]["traffic"]))/2
 
             if int(rain[i])/2 - int(lis_final[i]["traffic"])/2 > ans_max:
                 ans = i
-                ans_max = int(rain[i])/2 - int(lis_final[i]["traffic"])/2
+                ans_max = (int(rain[i]) - int(lis_final[i]["traffic"]))/2
 
             i += 1 
 
-    print(ans, ans_max)
-    #return render_template('home.html', lis_final = lis_final)
     oof = str(lis_final[ans]["cood"][0]) + ', ' + str(lis_final[ans]["cood"][1])
     return render_template('details.html', city = lis_final[ans]["place"], cood = lis_final[ans]["cood"], rain = rain[ans], traffic = int(lis_final[ans]["traffic"]), oof = oof)
 
